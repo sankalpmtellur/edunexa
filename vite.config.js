3c04c70cd09cb2cb2d16541cb97814a0c5931753
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-07-06 00:21:40 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/EduNexa/',
+  base: isProduction ? '/EduNexa/' : '/',
   server: {
     host: true,
     port: 5173,
