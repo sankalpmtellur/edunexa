3d2c5b89fc76010646bfc6a6cfc678d14e5a6979
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-07-05 12:59:51 +0530

    Third Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/edunexa/',
+  base: '/EduNexa/',
   server: {
     host: true,
     port: 5173,
