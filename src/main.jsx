663876c300a19636c0c337cbeba59a3cdd996ceb
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-07-06 00:25:57 +0530

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="273619274278-kbnbfsocjmao3rc4s6lmjkpkpsu52drh.apps.googleusercontent.com">
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
